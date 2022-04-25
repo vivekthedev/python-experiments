8c9414cb9f3bc9e49c4b4aa7f76473c735003be0
Vivek Kumar Singh <vivekhappy786@gmail.com>
2022-04-25 10:11:37 +0530

    enhancement(slideshare scrape): add progress bars and info msgs

--- Download Slideshare PPT/slideshare_scraping.py
+++ Download Slideshare PPT/slideshare_scraping.py
@@ -2,26 +2,80 @@ import requests
 from bs4 import BeautifulSoup
 from PIL import Image
 import io
+from termcolor import colored
+from colorama import init
+import sys
+import tqdm
+import os
 
-URL_LESS = "https://www.slideshare.net/angelucmex/global-warming-2373190?qid=8f04572c-48df-4f53-b2b0-0eb71021931c&v=&b=&from_search=1"
-URL = "https://www.slideshare.net/tusharpanda88/python-basics-59573634?qid=03cb80ee-36f0-4241-a516-454ad64808a8&v=&b=&from_search=5"
-r = requests.get(URL_LESS)
+init()
 
-soup = BeautifulSoup(r.content, "html5lib")
+cover = "Free Slideshare"
+c_cover = colored(cover, "green")
+print(c_cover)
+ask = colored("Paste your Link here ", "red")
+print(ask)
+link = input("")
 
-imgs = soup.find_all("img", class_="slide-image")
-imgSRC = [
-    x.get("srcset").split(",")[-2].strip().split(" ")[0].split("?")[0] for x in imgs
-]
+if link:
 
+    try:
+        r = requests.get(link)
+    except requests.exceptions.MissingSchema:
+        msg = colored("please paste a valid link", "red")
+        print(msg)
+        print("Press any key to Exit")
+        input("")
 
-imagesJPG = []
-for img in imgSRC:
-    im = requests.get(img)
-    f = io.BytesIO(im.content)
-    imgJPG = Image.open(f)
-    imagesJPG.append(imgJPG)
+    else:
+        soup = BeautifulSoup(r.content, "html5lib")
+        title = str(soup.title.string)
+        msg = colored("fetching link content...", "blue")
+        print(msg)
 
-imagesJPG[0].save(
-    f"{soup.title.string}.pdf", save_all=True, append_images=imagesJPG[1:]
-)
+        imgs = soup.find_all("img", class_="slide-image")
+        if len(imgs) < 1:
+            msg = colored(
+                "looks like the powerpoint does not exists, please try a different link",
+                "red",
+            )
+            print(msg)
+            print("Press any key to Exit")
+
+            input("")
+            sys.exit(1)
+
+        msg = colored("stripping slides link...", "green")
+        print(msg)
+
+        imgSRC = [
+            x.get("srcset").split(",")[-2].strip().split(" ")[0].split("?")[0]
+            for x in imgs
+        ]
+        msg = f"found {len(imgSRC)} slides "
+        print(msg)
+        imagesJPG = []
+        for img in tqdm.tqdm(imgSRC):
+            try:
+                im = requests.get(img)
+                f = io.BytesIO(im.content)
+            except:
+                msg = colored(
+                    "looks like your network connection is the culprit here, connect to a better connection then try again.",
+                    "red",
+                )
+                print(msg)
+                print("Press any key to Exit")
+                input("")
+                sys.exit(1)
+            else:
+                imgJPG = Image.open(f)
+                imagesJPG.append(imgJPG)
+
+        title = title + ".pdf"
+        imagesJPG[0].save(title, save_all=True, append_images=imagesJPG[1:])
+        msg = f"file saved at {os.getcwd()}/{title}"
+        msg = colored(msg, "green")
+        print(msg)
+        print("Press any key to Exit")
+        input("")
